b95028ecbc9f30844b103fcdd0a95e6307018c00
OleksiiDP <102666503+OleksiiDP@users.noreply.github.com>
2022-03-30 15:31:35 +0300

    calculate

--- calculate.java
+++ calculate.java
@@ -2,7 +2,10 @@ public class calculate {
 
 	public static void main(String[] agr) {
 		System.out.println("calculate");
-
+		int first = Integer.valueOf(agr[0]);
+		int second = Integer.valueOf(agr[1]);
+		int sum = first + second;
+		System.out.println("sum " + sum);
 	}
 
 
